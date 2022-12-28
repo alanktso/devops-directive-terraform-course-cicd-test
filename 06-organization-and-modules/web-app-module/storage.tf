d5795f06b641f8ee755a647cb81f7c4b65fed418
Alan So <alan.ktso@gmail.com>
2022-12-28 09:56:44 +0800

    Disable default S3 bucket for backend Change database engine version Change bucket option syntax

--- 06-organization-and-modules/web-app-module/storage.tf
+++ 06-organization-and-modules/web-app-module/storage.tf
@@ -1,15 +1,21 @@
 resource "aws_s3_bucket" "bucket" {
   bucket        = var.bucket_name
   force_destroy = true
-  versioning {
-    enabled = true
-  }
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_versioning" "versioning_bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
     }
   }
 }
--- 06-organization-and-modules/web-app/main.tf
+++ 06-organization-and-modules/web-app/main.tf
@@ -2,7 +2,8 @@ terraform {
   # Assumes s3 bucket and dynamo DB table already set up
   # See /code/03-basics/aws-backend
   backend "s3" {
-    bucket         = "devops-directive-tf-state"
+    # Specify bucket using -backend-config during terraform init
+    # bucket         = "devops-directive-tf-state"
     key            = "06-organization-and-modules/web-app/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-locking"
@@ -33,11 +34,19 @@ variable "db_pass_2" {
   sensitive   = true
 }
 
+resource "random_uuid" "uuid"{
+
+}
+
+locals {
+  webapp1_bucket = "webapp1-bucket-${random_uuid.uuid.result}"
+  webapp2_bucket = "webapp2-bucket-${random_uuid.uuid.result}"
+}
 module "web_app_1" {
   source = "../web-app-module"
 
   # Input Variables
-  bucket_name      = "web-app-1-devops-directive-web-app-data"
+  bucket_name      = local.webapp1_bucket
   domain           = "devopsdeployed.com"
   app_name         = "web-app-1"
   environment_name = "production"
@@ -52,7 +61,7 @@ module "web_app_2" {
   source = "../web-app-module"
 
   # Input Variables
-  bucket_name      = "web-app-2-devops-directive-web-app-data"
+  bucket_name      = local.webapp2_bucket
   domain           = "anotherdevopsdeployed.com"
   app_name         = "web-app-2"
   environment_name = "production"
